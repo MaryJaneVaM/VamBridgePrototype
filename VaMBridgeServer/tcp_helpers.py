9c8bd10d4c0874def094868c8814a41cbc3292a4
MaryJaneVaM <251234974+MaryJaneVaM@users.noreply.github.com>
2025-12-24 12:01:13 +0100

    Update and rename tcp_helpers.py to tcp_helpers.py

--- VaMBridgeServer/tcp_helpers.py
+++ VaMBridgeServer/tcp_helpers.py
@@ -9,7 +9,7 @@
 # you give appropriate credit and distribute your contributions under the same
 # license. Full license: https://creativecommons.org/licenses/by-nc-sa/4.0/
 #
-# Path: server/tcp_helpers.py
+# Path: VaMBridgeServer/tcp_helpers.py
 # -----------------------------------------------------------------------------
 
 """
